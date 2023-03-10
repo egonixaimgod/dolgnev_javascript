2e72e3d98c0c4ed34ed7189d7217f7d68cf0436f
egonixaimgod <bodnarbence2002@gmail.com>
2023-03-10 10:47:06 +0100

    bruh

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const tbody = document.querySelector("#tbody");
+
+var dolgozolista = [
+    {name : "Pisti", city: "Szolnok", salary: 385},
+    {name : "Marti", city: "Szolnok", salary: 385},
+    {name : "Kati", city: "Szolnok", salary: 385},
+    {name : "Dani", city: "Szolnok", salary: 385},
+    {name : "Lili", city: "Szolnok", salary: 385}
+];
+
+console.log(tbody)
